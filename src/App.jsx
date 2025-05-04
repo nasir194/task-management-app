2213a362b8254d62f9dc827639dd64eaa7c9e6ae
nasir194 <kazinasir194@gmail.com>
2025-05-05 01:20:44 +0600

    add Task feature done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import TaskBoard from "./Components/TaskBoard/TaskBoard";
 
 export default function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="flex flex-col justify-center items-center">
         <HeroSection />
         <TaskBoard />
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/Components/TaskBoard/TaskBoard.jsx
+++ src/Components/TaskBoard/TaskBoard.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react";
 import { defaultTasks } from "../../utility/data";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
   const [tasks, setTasks] = useState(defaultTasks); // Initialize tasks with defaultTasks
-  console.log(tasks);
+  //   console.log(tasks);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    const finalTask = { ...newTask, id: tasks.length + 1 };
+    setTasks([...tasks, finalTask]); // Add the new task to the existing tasks
+    setShowModal(false); // Close the modal after saving
+    // setTasks((prevTasks) => [...prevTasks, newTask]); // Add the new task to the existing tasks
+    // Assign a new ID based on the current length of tasks
+  };
 
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal onSave={handleAddTask} />}
+
       <div className="container">
         {/* <!-- Search Box --> */}
-        <SearchTask />
+        <div className="p-2 flex justify-end">
+          <SearchTask />
+        </div>
+
         {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           {/* <-- Task Actions start --> */}
-          <TaskActions />
+          <TaskActions onAddModal={() => setShowModal(true)} />
           {/* <-- Task Actions End --> */}
 
           {/* <!-- TaskList Table begin--> */}
